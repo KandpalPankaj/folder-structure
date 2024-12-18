c96eeb47c98765b1c250617dab276f441f47451a
Pankaj Kandpal <97446286+KandpalPankaj@users.noreply.github.com>
2024-12-18 17:45:04 +0530

    bug fix

---
+++ src/components/InputField.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const InputField = ({
+  defaultValue = "",
+  onSubmit,
+  onCancel,
+  placeholder,
+  isFolder,
+}) => {
+  const [value, setValue] = useState(defaultValue);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && value.trim()) {
+      onSubmit(value);
+    } else if (e.key === "Escape") {
+      onCancel();
+    }
+  };
+
+  return (
+    <div className="flex items-center space-x-2 my-1">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={handleKeyDown}
+        onBlur={onCancel}
+        className="border-b-2 border-blue-500 outline-none flex-grow px-1"
+        autoFocus
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default InputField;
